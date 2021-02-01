cfccc0f3e691bd6cffb5eb31534890229a90f67e
lautaro <lautaroecarro@gmail.com>
2021-02-01 07:15:40 -0300

    Update libraries versions

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -12,34 +12,38 @@ const Map: React.FC<MapProps> = ({ mapCenter, locations }) => {
 
   useEffect(() => {
 
-    map.current = new google.maps.Map(mapEle.current, {
-      center: {
-        lat: mapCenter.lat,
-        lng: mapCenter.lng
-      },
-      zoom: 16
-    });
+    if (mapEle && mapEle.current) {
+      map.current = new google.maps.Map(mapEle.current, {
+        center: {
+          lat: mapCenter.lat,
+          lng: mapCenter.lng
+        },
+        zoom: 16
+      });
 
-    addMarkers();
 
-    google.maps.event.addListenerOnce(map.current, 'idle', () => {
-      if (mapEle.current) {
-        mapEle.current.classList.add('show-map');
-      }
-    });
+
+      addMarkers();
+
+      google.maps.event.addListenerOnce(map.current, 'idle', () => {
+        if (mapEle.current) {
+          mapEle.current.classList.add('show-map');
+        }
+      });
+    }
 
     function addMarkers() {
       locations.forEach((markerData) => {
         let infoWindow = new google.maps.InfoWindow({
           content: `<h5>${markerData.name}</h5>`
         });
-  
+
         let marker = new google.maps.Marker({
           position: new google.maps.LatLng(markerData.lat, markerData.lng),
           map: map.current!,
           title: markerData.name
         });
-  
+
         marker.addListener('click', () => {
           infoWindow.open(map.current!, marker);
         });
