ce297992b6650047dd41b8a1b87859a20cf36a1a
lautaro <lautaroecarro@gmail.com>
2021-02-05 03:57:00 -0300

    add ionic-super-tabs

--- src/pages/BusinessDetail.tsx
+++ src/pages/BusinessDetail.tsx
@@ -8,6 +8,7 @@ import './BusinessDetail.scss';
 import { addFavorite, removeFavorite } from '../data/sessions/sessions.actions';
 import { Business } from '../models/Business';
 import AboutPopover from '../components/AboutPopover';
+import { SuperTab, SuperTabButton, SuperTabs, SuperTabsContainer, SuperTabsToolbar } from '@ionic-super-tabs/react';
 
 interface OwnProps extends RouteComponentProps { };
 
@@ -84,7 +85,6 @@ const BusinessDetail: React.FC<BusinessDetailProps> = ({ business, favoriteSessi
           </IonToolbar>
         </IonHeader>
 
-
         <IonSlides pager={true} className="business-detail-header" options={slideOpts}>
           <IonSlide>
 
@@ -103,65 +103,92 @@ const BusinessDetail: React.FC<BusinessDetailProps> = ({ business, favoriteSessi
           </IonSlide>
         </IonSlides >
 
-        <div className="business-detail-info">
+        <div className="business-detail-title">
           <h3 className="ion-padding-top ion-padding-start">{business.name}</h3>
           <p className="ion-padding-start">
             {business.address}
           </p>
-          <h3 className="ion-padding-top ion-padding-start">Descripción</h3>
-          <p className="ion-padding-start ion-padding-end">
-            The Ionic Conference is a one-day conference on {displayDate(conferenceDate, 'mediumDate')} featuring talks from the Ionic team. It is focused on Ionic applications being built with Ionic Framework. This includes migrating apps to the latest version of the framework, Angular concepts, Webpack, Sass, and many other technologies used in Ionic 2. Tickets are completely sold out, and we’re expecting more than 1000 developers – making this the largest Ionic conference ever!
-          </p>
-          
-          <h3 className="ion-padding-top ion-padding-start">Details</h3>
-
-          <IonList lines="none">
-            <IonItem>
-              <IonLabel>
-                Location
-              </IonLabel>
-              <IonSelect value={location} interfaceOptions={selectOptions} onIonChange={(e) => setLocation(e.detail.value as any)}>
-                <IonSelectOption value="madison">Madison, WI</IonSelectOption>
-                <IonSelectOption value="austin">Austin, TX</IonSelectOption>
-                <IonSelectOption value="chicago">Chicago, IL</IonSelectOption>
-                <IonSelectOption value="seattle">Seattle, WA</IonSelectOption>
-              </IonSelect>
-            </IonItem>
-            <IonItem>
-              <IonLabel>
-                Date
-              </IonLabel>
-              <IonDatetime
-                displayFormat="MMM DD, YYYY"
-                max="2056"
-                value={conferenceDate}
-                onIonChange={(e) => setConferenceDate(e.detail.value as any)}>
-              </IonDatetime>
-            </IonItem>
-          </IonList>
-
-          <h3 className="ion-padding-top ion-padding-start">Internet</h3>
-
-          <IonList lines="none">
-            <IonItem>
-              <IonLabel>
-                Wifi network
-              </IonLabel>
-              <IonLabel className="ion-text-end">
-                ica{displayDate(conferenceDate, 'y')}
-              </IonLabel>
-            </IonItem>
-            <IonItem>
-              <IonLabel>
-                Password
-              </IonLabel>
-              <IonLabel className="ion-text-end">
-                makegoodthings
-              </IonLabel>
-            </IonItem>
-          </IonList>
-
         </div>
+
+        <SuperTabs>
+          <SuperTabsToolbar color="white">
+            <SuperTabButton>
+              <IonLabel>Servicios</IonLabel>
+            </SuperTabButton>
+            <SuperTabButton>
+              <IonLabel>Opiniones</IonLabel>
+            </SuperTabButton>
+            <SuperTabButton>
+              <IonLabel>Detalles</IonLabel>
+            </SuperTabButton>
+          </SuperTabsToolbar>
+          <SuperTabsContainer>
+            <SuperTab noScroll={false}>
+              Im a custom page
+    </SuperTab>
+            <SuperTab noScroll={false}>
+              Im a other page
+            </SuperTab>
+            <SuperTab noScroll={false}>
+              <div className="business-detail-info">
+                <h3 className="ion-padding-top ion-padding-start">Descripción</h3>
+                <p className="ion-padding-start ion-padding-end">
+                  The Ionic Conference is a one-day conference on {displayDate(conferenceDate, 'mediumDate')} featuring talks from the Ionic team. It is focused on Ionic applications being built with Ionic Framework. This includes migrating apps to the latest version of the framework, Angular concepts, Webpack, Sass, and many other technologies used in Ionic 2. Tickets are completely sold out, and we’re expecting more than 1000 developers – making this the largest Ionic conference ever!
+          </p>
+
+                <h3 className="ion-padding-top ion-padding-start">Details</h3>
+
+                <IonList lines="none">
+                  <IonItem>
+                    <IonLabel>
+                      Location
+              </IonLabel>
+                    <IonSelect value={location} interfaceOptions={selectOptions} onIonChange={(e) => setLocation(e.detail.value as any)}>
+                      <IonSelectOption value="madison">Madison, WI</IonSelectOption>
+                      <IonSelectOption value="austin">Austin, TX</IonSelectOption>
+                      <IonSelectOption value="chicago">Chicago, IL</IonSelectOption>
+                      <IonSelectOption value="seattle">Seattle, WA</IonSelectOption>
+                    </IonSelect>
+                  </IonItem>
+                  <IonItem>
+                    <IonLabel>
+                      Date
+              </IonLabel>
+                    <IonDatetime
+                      displayFormat="MMM DD, YYYY"
+                      max="2056"
+                      value={conferenceDate}
+                      onIonChange={(e) => setConferenceDate(e.detail.value as any)}>
+                    </IonDatetime>
+                  </IonItem>
+                </IonList>
+
+                <h3 className="ion-padding-top ion-padding-start">Internet</h3>
+
+                <IonList lines="none">
+                  <IonItem>
+                    <IonLabel>
+                      Wifi network
+              </IonLabel>
+                    <IonLabel className="ion-text-end">
+                      ica{displayDate(conferenceDate, 'y')}
+                    </IonLabel>
+                  </IonItem>
+                  <IonItem>
+                    <IonLabel>
+                      Password
+              </IonLabel>
+                    <IonLabel className="ion-text-end">
+                      makegoodthings
+              </IonLabel>
+                  </IonItem>
+                </IonList>
+
+              </div>
+            </SuperTab>
+          </SuperTabsContainer>
+        </SuperTabs>
+
       </IonContent>
 
       <IonPopover
